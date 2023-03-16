6795f020e36d1dfb594374c1c561f1c22f959ae8
EnderNight <ndxendernight@gmail.com>
2023-03-17 00:20:52 +0100

    Added default ui, still not working

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	gcc -o yt-dlp-gui `pkg-config --cflags --libs gtk4` main.c
-
-.PHONY: clean
-
-clean:
-	rm -f yt-dlp-gui
--- main.c
+++ main.c
@@ -6,10 +6,12 @@ void activate(GtkApplication *app, gpointer user_data) {
     printf("activate called\n");
 
     GtkBuilder *builder;
-    GObject *window;
+    GtkWidget *window;
     
     builder = gtk_builder_new_from_file("ui.glade");
-    window = gtk_builder_get_object(builder, "main_window");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+
+    gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
 
     gtk_window_present(GTK_WINDOW(window));
 
@@ -24,7 +26,7 @@ int main(int argc, char** argv) {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("org.yt_dlp_gui", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("com.github.EnderNight.yt_dlp_gui", G_APPLICATION_DEFAULT_FLAGS);
 
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
